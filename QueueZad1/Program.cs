6883f63186b5e8f0aee6ae489a9c5bc0c2fe6398
Kameleon <kameleon@localhost.localdomain>
2021-01-04 13:10:36 +0100

    Initial commit

---
+++ QueueZad1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QueueZad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //elementi u redu se uklanjaju po redosledu u kom su dodati 
+            Queue<string> redBanaka = new Queue<string>();
+            redBanaka.Enqueue("Nikola");
+            redBanaka.Enqueue("Stefan");
+            redBanaka.Enqueue("Milos");
+
+            foreach (var item in redBanaka)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("=========================================");
+            Console.WriteLine("   Prozvan u banci: " + redBanaka.Dequeue());
+            Console.WriteLine("=========================================");
+            
+            foreach (var item in redBanaka)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
